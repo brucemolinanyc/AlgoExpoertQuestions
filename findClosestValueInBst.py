4ea8b04a0cb428b6ef9f98b2921a317baa8902fd
brucemolinanyc <brucemolinanyc@gmail.com>
2020-04-15 16:59:01 -0400

    updated solution

--- findClosestValueInBst.py
+++ findClosestValueInBst.py
@@ -10,5 +10,36 @@ less than or equal to the values of every node to its right; and its children no
 or None/Null
 """
 
+# recursive solution 
 def findClosestValueInBST(tree, target):
-    pass 
+    return findClosestValue(tree, target, float("inf"))
+
+
+def findClosestValue(tree, target, closest):
+    if tree is None:
+        return closest
+    if abs(target - closest) > abs(target - tree.value):
+        closest = tree.value
+    if target < tree.value:
+        return findClosestValueInBST(tree.left, target, closest)
+    elif target > tree.value:
+        return findClosestValueInBST(tree.right, target, closest)
+    else: 
+        return closest
+
+
+
+
+#iterative solution
+def findClosestValue(tree, target, closest):
+    currentNode = tree 
+    while currentNode is not None:
+        if abs(target - closest) > abs(target - currentNode.value):
+            closest = currentNode.value
+        if target < currentNode.value:
+            currentNode = currentNode.left
+        elif target > currentNode.value:
+            currentNode = currentNode.right 
+        else: 
+            return closest
+
